768bbf876bef06a7ff32cf7b45ef82a74e9bf1d8
arielpetit <Ariel.Tchikaya@adorsys.com>
2026-04-07 13:43:33 +0100

    feat(): fixed invitation

--- frontend/src/services/organizations/organizationRepository.ts
+++ frontend/src/services/organizations/organizationRepository.ts
@@ -53,7 +53,7 @@ export const organizationRepository = {
           requestBody: {
             name: organization.name,
             domains: [{ name: organization.domain }],
-            redirectUrl: "http://localhost:8000/",
+            redirectUrl: `${import.meta.env.VITE_APP_PUBLIC_URL ?? "https://158.220.84.249"}/`,
             enabled: "true",
             attributes: {
               description: [organization.description],
