9dd7255ab2ac467777e53e3fbd0de378c5646994
arielpetit <Ariel.Tchikaya@adorsys.com>
2026-04-13 10:27:09 +0100

    feat(): fixed organiation final

--- frontend/src/hooks/reports/useGenerateAndExportReport.ts
+++ frontend/src/hooks/reports/useGenerateAndExportReport.ts
@@ -16,7 +16,7 @@ export const useGenerateAndExportReport = () => {
       const baseUrl = OpenAPI.BASE || "";
 
       const response = await fetch(
-        `${baseUrl}/v1/reports/assessment/${assessmentId}/generate-and-export`,
+        `${baseUrl}/reports/assessment/${assessmentId}/generate-and-export`,
         {
           method: "POST",
           headers: {
